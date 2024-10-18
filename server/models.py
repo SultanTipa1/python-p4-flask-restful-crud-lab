57a542679b6b4f4b0f541b3f4648fbebcf074e18
SultanTipa1 <josephngumbaugush@gmail.com>
2024-10-18 19:55:14 +0300

    Adds all files

--- server/models.py
+++ server/models.py
@@ -12,5 +12,23 @@ class Plant(db.Model, SerializerMixin):
     price = db.Column(db.Float)
     is_in_stock = db.Column(db.Boolean)
 
+    def __init__(self, name, image=None, price=0.0, is_in_stock=True):
+        self.name = name
+        self.image = image
+        self.price = price
+        self.is_in_stock = is_in_stock
+
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+
+    def to_dict(self):
+        """Convert the Plant instance to a dictionary for serialization."""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+            'is_in_stock': self.is_in_stock
+        }
+
+    
